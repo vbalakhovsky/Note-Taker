180c48f1d222a04dbee5200daa950eaa8e1e054c
Veronika Balakhovsky <vbalakhovsky32@gmail.com>
2021-02-01 17:59:32 -0500

    server works

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,14 @@
-var express = require("express");
+const express = require("express");
 const htmlRoutes = require("./routes/htmlRoute");
-const apiRoutes = require(".routes/apiRoute");
-var app = express();
+const apiRoutes = require("./routes/apiRoute");
 
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8089;
 
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use(express.json());
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.listen(PORT,()=> console.log(`Listening on PORT : ${PORT}`));
+app.listen(PORT,()=> console.log(`Listening : ${PORT}`));
